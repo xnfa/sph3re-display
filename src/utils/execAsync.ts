ecd6889856cfea9d5318466f4b2cca6e525be1cb
xnfa2000 <xnfa2000@gmail.com>
2023-02-11 03:04:49 +0800

    fix crash issue when path with brackets

--- src/utils/execAsync.ts
+++ src/utils/execAsync.ts
@@ -2,7 +2,7 @@ import { exec } from 'child_process';
 
 export async function execAsync(command: string) {
   return await new Promise((resolve, reject) => {
-    exec(command, (err, stdout, stderr) => {
+    exec(`"${command}"`, (err, stdout, stderr) => {
       if (err) {
         console.log(stdout || stderr);
         return reject(err);
